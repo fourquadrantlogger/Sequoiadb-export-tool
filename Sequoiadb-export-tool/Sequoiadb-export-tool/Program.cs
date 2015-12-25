212002328c3979d239b92039d1e042381cdd521c
timeloveboy <734991033@qq.com>
2015-12-25 18:09:37 +0800

    Signed-off-by: timeloveboy <734991033@qq.com>

---
+++ Sequoiadb-export-tool/Sequoiadb-export-tool/Program.cs
@@ -0,0 +1,69 @@
+﻿using SequoiaDB;
+using System.IO;
+
+namespace Sequoiadb_export_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            usage();
+            doing(args);
+            while(true)
+            {
+                System.Console.Read();
+            }
+        }
+        static void doing(string[] args)
+        {
+            string outfolderpath = System.Environment.CurrentDirectory;
+            if (args.Length >= 1)
+                outfolderpath = args[0];
+            SequoiaDBHelper sh = new SequoiaDBHelper("10.249.109.185:50002");
+
+
+            foreach (string c in sh.ListCollections())
+            {
+                FileStream fs = new FileStream(outfolderpath + "/" + c + ".json", FileMode.Create);
+                fs.WriteByte((byte)'[');
+                int count = 0;
+                DBCursor cursor = sh.findRow(
+                     c.Substring(0, c.IndexOf('.')),
+                     c.Substring(c.IndexOf('.') + 1),
+                     new SequoiaDB.Bson.BsonDocument(),
+                     new SequoiaDB.Bson.BsonDocument(),
+                     new SequoiaDB.Bson.BsonDocument(),
+                     new SequoiaDB.Bson.BsonDocument()
+                     );
+                if (cursor.Current() != null)
+                {
+                    byte[] bs = getbs(cursor.Current());
+                    fs.Write(bs, 0, bs.Length);
+                    count++;
+                }
+                while (cursor.Next() != null)
+                {
+                    fs.WriteByte((byte)',');
+                    byte[] bs = getbs(cursor.Current());
+                    fs.Write(bs, 0, bs.Length);
+                    count++;
+                }
+                fs.WriteByte((byte)']');
+               
+                System.Console.WriteLine("write OK" + '\t'+ outfolderpath + "/" + c + ".json" + "\n json count:"+ count + "\t" + fs.Length + " bytes");
+                fs.Flush();
+                fs.Close();
+            }
+        }
+        static byte[] getbs(SequoiaDB.Bson.BsonDocument db)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(db.ToString());
+        }
+        static string usage()
+        {
+
+            return "usage:"+
+                "Sequoiadb-export-tool.exe D://mySequoiadbPath";
+        }
+    }
+}
---
+++ Sequoiadb-export-tool/Sequoiadb-export-tool/SequoiaDBHelper.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SequoiaDB;
+using SequoiaDB.Bson;
+using System.Configuration;
+
+
+
+namespace Sequoiadb_export_tool
+{
+    public class SequoiaDBHelper
+    {
+        public SequoiaDBHelper(string db_url)
+        {
+            Connect(db_url);
+
+        }
+        Sequoiadb sdb;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Sequoiadb Sdb
+        {
+            get
+            {
+                return sdb;
+            }
+        }
+
+        private bool Connect(string addr)
+        {
+            sdb = new Sequoiadb(addr);
+            try
+            {
+                Sdb.Connect();
+            }
+            catch (BaseException e)
+            {
+                Console.Error.WriteLine("ErrorCode:{0}, ErrorType:{1}", e.ErrorCode, e.ErrorType);
+                Console.Error.WriteLine(e.Message);
+                return false;
+            }
+            catch (System.Exception e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
+                return false;
+            }
+
+            return true;
+        }
+        public List<string> ListCollectionSpaces()
+        {
+            DBCursor cursor = sdb.ListCollectionSpaces();
+            List<string> result = new List<string>();
+
+            if (cursor.Current() != null) result.Add((string)(cursor.Current()["Name"]));
+            while (cursor.Next() != null)
+            {
+                result.Add((string)(cursor.Current()["Name"]));
+            }
+            return result;
+        }
+        public List<string> ListCollections()
+        {
+            DBCursor cursor = sdb.ListCollections();
+            List<string> result = new List<string>();
+
+            if (cursor.Current() != null) result.Add((string)(cursor.Current()["Name"]));
+            while (cursor.Next() != null)
+            {
+                result.Add((string)(cursor.Current()["Name"]));
+            }
+            return result;
+        }
+        private CollectionSpace GetCollectionSpace(string csName)
+        {
+            CollectionSpace cs;
+            if (sdb.IsCollectionSpaceExist(csName))
+            {
+                cs = sdb.GetCollecitonSpace(csName);
+
+                return cs;
+            }
+            else {
+                cs = sdb.CreateCollectionSpace(csName);
+                return cs;
+            }
+        } 
+
+        private DBCollection GetCollection(string csName, string clName)
+        {
+            CollectionSpace cs = GetCollectionSpace(csName);
+            DBCollection dbc;
+            if (cs.IsCollectionExist(clName)) { 
+                dbc = cs.GetCollection(clName);
+                return dbc;
+            }
+            else {
+                 dbc = cs.CreateCollection(clName);
+                return dbc;
+            }
+        }
+        private void CreateIndex(ref DBCollection dbc, string Indexname,BsonDocument key)
+        {
+            bool isUnique = true;
+            bool isEnforced = true;
+            dbc.CreateIndex(Indexname, key, isUnique, isEnforced);
+        }
+        public BsonValue insertRow(string CollectionSpaceName, string CollectionName, BsonDocument insertor)
+        {
+            DBCollection dbc=GetCollection(CollectionSpaceName, CollectionName);
+            BsonValue  result=dbc.Insert(insertor);            
+            return result;
+        }
+
+        public BsonValue insertRowWithClose(string CollectionSpaceName, string CollectionName, BsonDocument insertor)
+        {
+            DBCollection dbc = GetCollection(CollectionSpaceName, CollectionName);
+            BsonValue result = dbc.Insert(insertor);
+            sdb.Disconnect();
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CollectionSpaceName"></param>
+        /// <param name="CollectionName"></param>
+        /// <param name="updater">修改值</param>
+        /// <param name="matcher">条件</param>
+        /// <param name=""></param>
+        public void updateRow(string CollectionSpaceName, string CollectionName, BsonDocument matcher, BsonDocument updater)
+        {
+            DBCollection dbc = GetCollection(CollectionSpaceName, CollectionName);
+            DBQuery query = new DBQuery();
+
+            BsonDocument modifier = new BsonDocument();
+            modifier.Add("$set", updater);
+
+            query.Matcher = matcher;
+            query.Modifier = modifier;
+            dbc.Update(query);
+        }
+        /// <summary>
+        /// while (cursor.Next() != null)
+        ///Console.WriteLine(cursor.Current());
+        /// </summary>
+        /// <param name="CollectionSpaceName"></param>
+        /// <param name="CollectionName"></param>
+        /// <param name="indexname"></param>
+        /// <param name="query"></param>
+        /// <param name="selector"></param>
+        /// <param name="orderBy"></param>
+        /// <param name=""></param>
+        /// <param name="hint">访问索引（index）</param>
+        public DBCursor findRow(string CollectionSpaceName, string CollectionName,BsonDocument query, BsonDocument selector, BsonDocument orderBy, BsonDocument hint)
+        {
+            DBCollection dbc = GetCollection(CollectionSpaceName, CollectionName);
+            DBCursor cursor = dbc.Query(query, selector, orderBy, hint, 0, -1);
+            cursor.Current();
+            return cursor;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CollectionSpaceName"></param>
+        /// <param name="CollectionName"></param>
+        /// <param name="query">查询匹配规则（query，包含相应的查询条件）</param>
+        /// <param name="selector">域选择（selector）</param>
+        /// <param name="orderBy">排序规则（orderBy，增序或降序）</param>
+        /// <param name="hint">制定访问计划（hint）</param>
+        /// <returns></returns>
+        public List<BsonDocument> findRowData(string CollectionSpaceName, string CollectionName, BsonDocument query, BsonDocument selector, BsonDocument orderBy, BsonDocument hint)
+        {
+            DBCollection dbc = GetCollection(CollectionSpaceName, CollectionName);
+            DBCursor cursor = dbc.Query(query, selector, orderBy, hint, 0, -1);
+            
+            List<BsonDocument> result = new List<BsonDocument>();
+            if(cursor.Current()!=null) result.Add(cursor.Current());
+            while (cursor.Next() != null)
+            {
+                result.Add(cursor.Current());
+            }
+            return result;
+        }
+
+        public int findRowCount(string CollectionSpaceName, string CollectionName, BsonDocument query, BsonDocument selector, BsonDocument orderBy, BsonDocument hint)
+        {
+            int count = 0;
+            DBCollection dbc = GetCollection(CollectionSpaceName, CollectionName);
+            DBCursor cursor = dbc.Query(query, selector, orderBy, hint, 0, -1);
+            if(cursor.Current()!= null)count++ ;
+            while (cursor.Next() != null)
+            {
+                count++;
+            }
+            return count;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CollectionSpaceName"></param>
+        /// <param name="CollectionName"></param>
+        /// <param name="matcher"></param>
+        /// <param name="modifier"></param>
+        /// <param name="hint"></param>
+        public void upsertRow(string CollectionSpaceName, string CollectionName, BsonDocument matcher, BsonDocument updater, BsonDocument hint)
+        {
+            DBCollection dbc = GetCollection(CollectionSpaceName, CollectionName);
+            BsonDocument modifier = new BsonDocument();
+            modifier.Add("$set", updater);
+            dbc.Upsert(matcher,modifier,hint);
+        }
+
+    }
+}
